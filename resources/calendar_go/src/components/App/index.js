7036567f31a3f2840e9b4c0d895beac536ca4dc9
DimKush <dimkysh@gmail.com>
2021-05-21 21:05:17 +0300

    resources-js | get longitude and latitude

--- resources/calendar_go/src/components/App/index.js
+++ resources/calendar_go/src/components/App/index.js
@@ -4,12 +4,37 @@ import {Header} from '../Header'
 import {Controls} from '../Controls'
 import {CalendarGrid} from '../Grid'
 
+let getLocationPromise = new Promise((resolve, reject) => {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        resolve({latitude: lat, longitude: long});
+    });
+  } else {
+        reject("your browser doesn't support geolocation API");
+  }
+});
+
 
 function App() {
   //TODO : in future from golang server getLocation and install weekends. Right now for the russian
+  
+  var latitude;
+  var longitude;
+
+  getLocationPromise.then((location) => {
+    latitude = location.latitude;
+    longitude = location.longitude;
+  }).catch((err) =>{
+    console.log(err);
+  });
+
   moment.updateLocale('en',{week:{dow:'1'}});
   var dateBegin = moment().startOf('month').startOf('week');
-
+  //console.log('eee', latitude, longitude);
+  
   return (
     <div>
     <Header />
